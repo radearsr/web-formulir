97d8d69c5083281ff624a1b306e2cc8b7a202fe7
radearsr <radea.surya1@gmail.com>
2021-04-14 09:45:14 +0700

    Ganti Title Pada Form PDF

--- source/print-pdf/laporanpdf.php
+++ source/print-pdf/laporanpdf.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas</title>
+    <title>Formulir</title>
     <link rel="stylesheet" href="../../assets/custom/css/print.pdf.css">
 </head>
 <body>
