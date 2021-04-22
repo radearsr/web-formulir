ffc1935968f4b67ca0caf653d02e4650a101ac21
radearsr <radea.surya1@gmail.com>
2021-04-23 05:20:00 +0700

    Menambahkan Fungsi update

--- source/functions.php
+++ source/functions.php
@@ -1,15 +1,13 @@
 <?php 
-
-// Koneksi Ke Databse
-$conn = mysqli_connect("localhost", "root", "", "tugas_magang" );
+include "config.php";
 
 // Fungsi Tambah Data Ke Database
-function insertData($no, $tanggal, $opd, $bidang, $alamat, $FirstTelp, $FirstMail, $NmAdmin, $NipAdmin, $JbtAdmin, $MailAdmin, $TelpAdmin, $NmTeknis, $NipTeknis, $JbtTeknis, $MailTeknis, $TelpTeknis, $merk, $ram, $hdd, $psu, $tipe, $proc, $core, $os, $sn, $appServer, $desServer, $nmTerang1, $nipTtd1, $nmTerang2, $nipTtd2){
+function insertData($tanggal, $opd, $bidang, $alamat, $FirstTelp, $FirstMail, $NmAdmin, $NipAdmin, $JbtAdmin, $MailAdmin, $TelpAdmin, $NmTeknis, $NipTeknis, $JbtTeknis, $MailTeknis, $TelpTeknis, $merk, $ram, $hdd, $psu, $tipe, $proc, $core, $os, $sn, $appServer, $desServer, $nmTerang1, $nipTtd1, $nmTerang2, $nipTtd2){
     global $conn;
 
     $query = "INSERT INTO data_print
                 VALUES
-        ('', '$no', '$tanggal', '$opd', '$bidang', '$alamat', '$FirstTelp', '$FirstMail', '$NmAdmin', '$NipAdmin', '$JbtAdmin', '$MailAdmin', '$TelpAdmin', '$NmTeknis', '$NipTeknis', '$JbtTeknis', '$MailTeknis', '$TelpTeknis', '$merk', '$ram', '$hdd', '$psu', '$tipe', '$proc', '$core', '$os', '$sn', '$appServer', '$desServer', '$nmTerang1', '$nipTtd1', '$nmTerang2', '$nipTtd2') 
+        ('', '$tanggal', '$opd', '$bidang', '$alamat', '$FirstTelp', '$FirstMail', '$NmAdmin', '$NipAdmin', '$JbtAdmin', '$MailAdmin', '$TelpAdmin', '$NmTeknis', '$NipTeknis', '$JbtTeknis', '$MailTeknis', '$TelpTeknis', '$merk', '$ram', '$hdd', '$psu', '$tipe', '$proc', '$core', '$os', '$sn', '$appServer', '$desServer', '$nmTerang1', '$nipTtd1', '$nmTerang2', '$nipTtd2') 
     ";
 
     mysqli_query($conn, $query);
@@ -53,6 +51,53 @@ function delete($id){
     return mysqli_affected_rows($conn);
 
 
+}
+
+// Fungsi untuk Update Data dari database
+function update($id, $tanggal, $opd, $bidang, $alamat, $FirstTelp, $FirstMail, $NmAdmin, $NipAdmin, $JbtAdmin, $MailAdmin, $TelpAdmin, $NmTeknis, $NipTeknis, $JbtTeknis, $MailTeknis, $TelpTeknis, $merk, $ram, $hdd, $psu, $tipe, $proc, $core, $os, $sn, $appServer, $desServer, $nmTerang1, $nipTtd1, $nmTerang2, $nipTtd2){
+    global $conn;
+
+    $query = (
+        " UPDATE data_print SET
+          tanggal       = '$tanggal',
+          opd           = '$opd',
+          bidang        = '$bidang',
+          alamat        = '$alamat',
+          firstTelp     = '$FirstTelp',
+          firstMail     = '$FirstMail',
+          nmAdmin       = '$NmAdmin',
+          nipAdmin      = '$NipAdmin',
+          jbtAdmin      = '$JbtAdmin', 
+          mailAdmin     = '$MailAdmin', 
+          telpAdmin     = '$TelpAdmin',
+          nmTeknis      = '$NmTeknis',
+          nipTeknis     = '$NipTeknis',
+          jbtTeknis     = '$JbtTeknis',
+          mailTeknis    = '$MailTeknis',
+          telpTeknis    = '$TelpTeknis',
+          merk          = '$merk',
+          ram           = '$ram',
+          hdd           = '$hdd',
+          psu           = '$psu',
+          tipe          = '$tipe',
+          proc          = '$proc',
+          core          = '$core',
+          os            = '$os',
+          sn            = '$sn',
+          appServer     = '$appServer',
+          desServer     = '$desServer',
+          nmTerang1     = '$nmTerang1',
+          nipTtd1       = '$nipTtd1',
+          nmTerang2     = '$nmTerang2',
+          nipTtd2       = '$nipTtd2'
+          WHERE id = '$id'
+        "
+    );
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+    // return mysqli_error($conn);
 }
 
 
