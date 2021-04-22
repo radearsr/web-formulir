a040c86fcbacf4c907d0db41f4a75eeff6e78346
radearsr <radea.surya1@gmail.com>
2021-04-23 05:15:12 +0700

    Menambahkan File config.php

---
+++ source/config.php
@@ -0,0 +1,7 @@
+<?php 
+// Koneksi Ke Databse Local
+$conn = mysqli_connect("localhost", "root", "", "tugas_magang" );
+
+// Koneksi Ke Databse Hosting
+// $conn = mysqli_connect("sql101.epizy.com", "epiz_28366446", "v0xgke60FY", "epiz_28366446_tugas_magang" );
+?>
